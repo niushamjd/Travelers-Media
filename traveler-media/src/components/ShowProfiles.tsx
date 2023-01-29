6e44528a02582bc3b77aa4492a9eaef7f1b66ffd
Niyousha Mojoudi <niushamjd@gmail.com>
2023-01-29 17:01:38 +0100

    Display users backend

--- traveler-media/src/components/ShowProfiles.tsx
+++ traveler-media/src/components/ShowProfiles.tsx
@@ -3,42 +3,51 @@ import profileImage from '../ProfilePicture.jpg';
 
 
 
-const countries = ['Paris', 'London', 'Prague', 'Sofia'];
 
 function App() {
   const [users, setUsers] = useState([]);
-  const fetchData = () => {
-    fetch(`${process.env.REACT_APP_BACKEND}api/users`)
-    .then(res => res.json())
-    .then(data => setUsers(data));
-  }
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(users);
+  async function fetchData() {
+    const response = await fetch(`${process.env.REACT_APP_BACKEND}api/users`)
+    const data = await response.json();
+    setUsers(data);
+  }
   
-  const [selectedcity, setSelectedcity] = useState('');
+  const [selectedcity, setSelectedcity] = useState('?');
+  
   const [filteredUsers, setFilteredUsers] = useState(users);
-/*
-  function handleSelect(event: { target: { value: React.SetStateAction<string>; }; }) {
+
+  
+  const cities = users.map(user => user['cityName']).filter((city, index, self) => self.indexOf(city) === index);
+
+  function handleSelect(event: { target: { value: any; }; }) {
+
+    
+    if(event.target.value === 'All') {
+      setSelectedcity('All');
+      setFilteredUsers(users);
+      return;
+    }
     setSelectedcity(event.target.value);
-    setFilteredUsers(users.filter(user => user.city === event.target.value));
-  }*/
+    setFilteredUsers(users.filter(user => user['cityName'] === event.target.value));
+  }
 
   return (
     <div>
       <label>
         Select a city:
-        <select value={selectedcity}>
-          <option value="">All</option>
-          {countries.map(city => (
+        <select value={selectedcity} onChange={handleSelect}>
+          <option value="All">All</option>
+          {cities.map(city => (
             <option key={city} value={city}>{city}</option>
           ))}
         </select>
       </label>
-      <h2>Users from {selectedcity}:</h2>
+      <h2>Users in {selectedcity}:</h2>
       <ul>
-      {users.map(user => (
+      {filteredUsers.map(user => (
         <div key={user['username']} style={{ display: "flex", alignItems: "left"}}>
         <img src={profileImage} alt={`${user['username']} profile picture`} style={{ width: '25px', height: '25px', marginRight: '10px' }} />
         <p>{user['username']}</p>
